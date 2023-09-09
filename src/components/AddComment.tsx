74a271e373a058fda3419d47c689747b1f74b90d
Sidhant <alairoet@gmail.com>
2023-09-09 22:34:04 +0530

    mention tasks

--- src/components/AddComment.tsx
+++ src/components/AddComment.tsx
@@ -4,8 +4,23 @@ interface CommentsSectionProps {
     handleAddComment: (i:string) => void;
 }
 
+const MENTIONABLE_NAMES = ["Sidhant", "tagmango" , "Aayush", "Krishna"]
+
 const AddComment = (props: CommentsSectionProps) => {
-    const [comment, setComment] = useState("")
+    const [comment, setComment] = useState("");
+    const spaceDelimitedComment = comment.split(" ");
+    const lastWord = spaceDelimitedComment[spaceDelimitedComment.length-1];
+    const [displayMentionList, setDisplayMentionList] = useState(false);
+
+    let commentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(e.target.value.split(" ")[e.target.value.split(" ").length-1].startsWith("@")){
+          setDisplayMentionList(true);
+        }
+        else
+          setDisplayMentionList(false);
+
+        setComment(e.target.value);
+    }
   return (
     <div>
         <h2>Add a Comment</h2>
@@ -13,9 +28,15 @@ const AddComment = (props: CommentsSectionProps) => {
         type="text"
         placeholder="Type your comment here"
         value={comment}
-        onChange={(e) => setComment(e.target.value)}
-      />
-      <button onClick={()=>props.handleAddComment(comment)}>Add Comment</button>
+        onChange={commentChange}
+      /><ul>
+        {displayMentionList && 
+        MENTIONABLE_NAMES.map(name => {
+          if(name.toLowerCase().startsWith(lastWord.substring(1).toLowerCase()))
+          return <li key={name} onClick={() => setComment(`${comment.split(lastWord)[0]}@${name}`)}>{name}</li>
+        })}
+      </ul>
+      <button onClick={()=>{props.handleAddComment(comment);setComment("");setDisplayMentionList(false)}}>Add Comment</button>
     </div>
   )
 }
